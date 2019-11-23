652cf2fe9f016c736330559414939c727972c539
mukulsikka <mukulsikka12@gmail.com>
2019-11-23 16:47:19 +0530

    finish

---
+++ web-interface/src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import './App.css';
+import Web3 from 'web3';
+
+import {
+  HashRouter as Router,
+  Route
+} from 'react-router-dom'
+
+import Channel from './components/Channel'
+import About from './components/About'
+import SideBar from './components/Sidebar'
+import NavBar from './components/NavBar'
+import Wallet from './components/Wallet'
+import Expenses from './components/Expenses'
+import ListGroup from './components/listgroup'
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+import { Input, Menu, Segment, Sidebar } from 'semantic-ui-react'
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    // Web3 providers
+    const web3 = new Web3(Web3.givenProvider || "http://kovan.infura.io");
+    const web3WH = new Web3();
+    const eventProvider = new Web3.providers.WebsocketProvider('ws://localhost:7545')
+    // const web3WH = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    web3WH.setProvider(eventProvider)
+
+    this.state = {
+      web3: web3,
+      web3WH: web3WH,
+      accounts: '',
+    };
+
+  }
+
+  render() {
+    return (
+
+
+      <Router>
+        <div>
+          <NavBar web3={this.state.web3} web3WH={this.state.web3WH} />
+          {/* <Sidebar.Pushable style={{margin: '0 !important'}} bottom attached> */}
+            <SideBar />
+
+            <Sidebar.Pusher >
+              <div style={{height: '50px'}}></div>
+              <Route exact path='/' render={() => <Channel web3={this.state.web3} web3WH={this.state.web3WH} />} />
+              <Route path='/create' render={() => <Channel web3={this.state.web3} web3WH={this.state.web3WH} />} />
+              <Route path='/view' render={() => <ListGroup web3={this.state.web3} web3WH={this.state.web3WH} />} />
+              <Route path="/about" component={About} />
+              <Route path='/expenses/:channelID' render={(props) => <Expenses web3={this.state.web3} web3WH={this.state.web3WH} match={props.match} />} />
+              <Route path="/wallet" render={() => <Wallet web3={this.state.web3} web3WH={this.state.web3WH} />} />
+            </Sidebar.Pusher>
+          {/* </Sidebar.Pushable> */}
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ web-interface/src/components/NavBar.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+
+import EthBalanceDisplay from './EthBalanceDisplay'
+import { BigNumber } from 'bignumber.js';
+import { Input, Menu, Segment, Button } from 'semantic-ui-react'
+import SETokenJSON from '../build/contracts/SEToken.json'
+import { NETWORK_ID } from './Channel';
+import { Link } from 'react-router-dom';
+
+
+class NavBar extends Component {
+
+  constructor(props) {
+    super(props);
+    const SETAddress = SETokenJSON.networks[NETWORK_ID].address;
+    const SETABI = SETokenJSON.abi;
+
+    const seToken = new props.web3.eth.Contract(SETABI, SETAddress);
+    const seToken_event = new props.web3WH.eth.Contract(SETABI, SETAddress);
+    seToken_event.setProvider(props.web3WH.currentProvider);
+
+    this.state = {
+      web3: props.web3,
+      web3WH: props.web3WH,
+      seToken: seToken,
+      seToken_event: seToken_event,
+      activeItem: 'home'
+    };
+
+    this.handleGetTokens = this.handleGetTokens.bind(this);
+
+  }
+
+  async componentDidMount() {
+    var _this = this;
+
+    var accounts;
+    this.state.web3.eth.getAccounts().then(res => {
+      accounts = res;
+      this.setState({ accounts: accounts });
+      this.state.seToken.methods.balanceOf(this.state.accounts[0]).call()
+      .then(function (res) {
+        _this.setState({ tokenBalance: _this.state.web3.utils.fromWei(res) });
+      });
+    })
+
+    this.state.seToken_event.events.Transfer({ fromBlock: 'latest', toBlock: 'latest' })
+    .on('data', event => {
+      //console.log("QQQ",event.returnValues._message);
+      _this.state.seToken.methods.balanceOf(_this.state.accounts[0]).call()
+      .then(function (res) {
+            _this.setState({ tokenBalance: _this.state.web3.utils.fromWei(res) });
+          });
+        });
+
+
+      }
+
+      async handleGetTokens() {
+        var _this = this;
+        await this.state.seToken.methods.getTokens(
+          this.state.accounts[0],
+          this.state.web3.utils.toWei("100000")
+        ).send({ from: this.state.accounts[0] })
+        .then(function (receipt) {
+          _this.state.seToken.methods.balanceOf(_this.state.accounts[0]).call()
+          .then(function (res) {
+            _this.setState({ tokenBalance: _this.state.web3.utils.fromWei(res) });
+          });
+        });
+      }
+
+      handleItemClick = (e, { name }) => {
+        window.location.href = "https://makerdao.com/"
+        this.setState({ activeItem: name })
+      }
+
+      render() {
+        return (
+        <Menu pointing inverted className="top fixed">
+          <Menu.Item name='home' active={this.state.activeItem === 'home'} onClick={this.handleItemClick} />
+
+          <Menu.Item
+            name='about'
+            active={this.state.activeItem === 'about'}
+            onClick={this.handleItemClick}
+            />
+
+          <Menu.Menu position='right'>
+            <Menu.Item>
+            <Button onClick={() => this.handleGetTokens()}> Get DAI ({this.state.tokenBalance} DAI) </Button>
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu>
+
+        )
+  }
+}
+
+export default NavBar;
